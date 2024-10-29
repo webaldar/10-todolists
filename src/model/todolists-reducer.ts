c199361d16eb344a201c8a45ff6a052e71d9be5c
webaldar <webaldar@gmail.com>
2024-10-29 14:54:23 +0300

    first commit

---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,64 @@
+import {FilterValuesType, TodolistType} from "../App";
+import {v1} from "uuid";
+
+
+let todolistID1 = v1()
+let todolistID2 = v1()
+
+const initialState: TodolistType[] = [
+	{id: todolistID1, title: 'What to learn', filter: 'all'},
+	{id: todolistID2, title: 'What to buy', filter: 'all'},
+]
+
+export const todolistsReducer = (state: TodolistType[] = initialState, action: ActionsType) => {
+	switch (action.type) {
+		case 'REMOVE-TODOLIST': {
+			return state.filter(tl => tl.id !== action.payload.id)
+		}
+
+		case 'ADD-TODOLIST': {
+			const newTodolist: TodolistType = {id: action.payload.todolistId, title: action.payload.title, filter: 'all'}
+			return [...state, newTodolist]
+		}
+
+		case 'CHANGE-TODOLIST-TITLE': {
+			return state.map(tl => tl.id === action.payload.id ? {...tl, title: action.payload.title} : tl)
+		}
+
+		case 'CHANGE-TODOLIST-FILTER': {
+			return state.map(tl => tl.id === action.payload.id ? {...tl, filter: action.payload.filter} : tl)
+		}
+
+		default:
+			throw new Error("I don't understand this type")
+	}
+}
+
+// Action creators
+export const removeTodolistAC = (todolistId: string) => {
+	return {type: 'REMOVE-TODOLIST', payload: {id: todolistId}} as const
+}
+
+export const addTodolistAC = (title: string) => {
+	return {type: 'ADD-TODOLIST', payload: {title, todolistId: v1()}} as const
+};
+
+export const changeTodolistTitleAC = (id: string, title: string) => {
+	return {type: 'CHANGE-TODOLIST-TITLE', payload: {id, title}} as const
+};
+
+export const changeTodolistFilterAC = (id: string, filter: FilterValuesType) => {
+	return {type: 'CHANGE-TODOLIST-FILTER', payload: {id, filter}} as const
+}
+
+// Actions types
+export type RemoveTodolistActionType = ReturnType<typeof removeTodolistAC>
+export type AddTodolistActionType = ReturnType<typeof addTodolistAC>
+export type ChangeTodolistTitleActionType = ReturnType<typeof changeTodolistTitleAC>
+export type ChangeTodolistFilterActionType = ReturnType<typeof changeTodolistFilterAC>
+
+type ActionsType = RemoveTodolistActionType
+	| AddTodolistActionType
+	| ChangeTodolistTitleActionType
+	| ChangeTodolistFilterActionType
+
