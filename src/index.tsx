8ec9bba591ba77a2643be82a7202baf293fcd459
webaldar <webaldar@gmail.com>
2024-10-30 15:28:39 +0300

    install redux and react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import AppWithReducer from "./AppWithReducer";
+import AppWithRedux from "./AppWithRedux";
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<AppWithReducer/>
+		<AppWithRedux/>
 	</React.StrictMode>
 );
 
