b5fe98b5a89f585cd7aee678ff1b2b47d2b06999
webaldar <webaldar@gmail.com>
2024-10-30 15:48:23 +0300

    intermediate result

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import {combineReducers, legacy_createStore} from "redux";
+import {tasksReducer} from "../model/tasks-reducer";
+import {todolistsReducer} from "../model/todolists-reducer";
+
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+})
+
+export const store = legacy_createStore(rootReducer)
+
+export type RootState = ReturnType<typeof store.getState>
+
+window.store = store
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import AppWithReducer from "./AppWithReducer";
 import AppWithRedux from "./AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "./app/store";
 
 const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<AppWithRedux/>
-	</React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <AppWithRedux/>
+        </Provider>
+    </React.StrictMode>
 );
 
 
